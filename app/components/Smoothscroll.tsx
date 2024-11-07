802f5c09b4659efb1f2fbd5b587bc85dd1f0365f
MaheTradejin <mahetradejini@gmail.com>
2024-11-07 19:17:50 +0530

    latest commit

--- app/components/Smoothscroll.tsx
+++ app/components/Smoothscroll.tsx
@@ -1,17 +1,15 @@
 "use client";
 
 import { ReactLenis } from "lenis/react";
+import { ReactNode } from "react";
 
-function SmoothScroll({ children }) {
+type SmoothScrollProps = {
+  children: ReactNode;
+};
+
+function SmoothScroll({ children }: SmoothScrollProps) {
   return (
-    <ReactLenis
-      root
-      options={{
-        lerp: 0.1,
-        duration: 1.5,
-        smoothWheel: true,
-      }}
-    >
+    <ReactLenis root>
       {children}
     </ReactLenis>
   );
